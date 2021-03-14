c6d4195c7700447fe6c93fee58c00dc15a71052f
Rythin <42111335+Rythin@users.noreply.github.com>
2021-03-14 03:00:57 -0700

    Worked on Tasks

--- analysis/Dante/scripts1/project_functions.py
+++ analysis/Dante/scripts1/project_functions.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
 import pandas as pd
 
-def load_and_process(path):    
-
-    load = (
-    pd.read_csv(path,header = None)
-    .rename(columns={0:"instant",1:"dteday",2:"season",3:"yr",4:"mnth",5:"hr",6:"holiday",7:"weekday",
-                     8:"workingday",9:"weathersit",10:"temp",11:"atemp",12:"hum",13:"windspeed",
-                     14:"casual",15:"registered",16:"cnt"})
-    .dropna()
-    .rename(index = lambda x: x + 1) 
-    )
-
-    return load
+def load_and_process(url):
+    df = (pd.read_csv(url)
+           .dropna()
+           .drop_duplicates()
+           .rename(columns={'dteday':'date'})
+           .sort_values(['date','hr']))
+    df["workingday"] = df["workingday"].astype(bool)
+    return df
